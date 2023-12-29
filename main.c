e392ede04682e66ccf8385a20d105e325c8e90b8
PAKFA57 <danil1001001@mail.ru>
2023-12-29 11:47:09 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.6.33723.286
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "main", "main\main.vcxproj", "{0327600D-A674-4A73-983D-962BABA50C8E}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Debug|x64.ActiveCfg = Debug|x64
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Debug|x64.Build.0 = Debug|x64
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Debug|x86.ActiveCfg = Debug|Win32
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Debug|x86.Build.0 = Debug|Win32
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Release|x64.ActiveCfg = Release|x64
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Release|x64.Build.0 = Release|x64
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Release|x86.ActiveCfg = Release|Win32
+		{0327600D-A674-4A73-983D-962BABA50C8E}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {6EB31E30-F80A-4968-B14C-098B48DFA6B4}
+	EndGlobalSection
+EndGlobal
